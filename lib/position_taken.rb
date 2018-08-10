05f9d3a29b90a91b79ea7711030ef798292eda59
karen S <buzz.kyt@gmail.com>
2018-08-10 20:13:55 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board,index)
   if board[index] != " " && board[index] != ""
     TRUE;
-  elsif board[index] == "nil" 
+  elsif board[index] == nil 
     false;
   else false;
   end
